5dd8c381a2ee241187f9e4210e870917d0d44d6f
Karema <karemahamdy51@gmail.com>
2025-02-09 00:21:02 +0200

    Queue

---
+++ Queue/implementation linked list.js
@@ -0,0 +1,37 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  front() {
+    return this.items.length > 0 ? this.items[0] : null;
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.dequeue()); // 10
+console.log(queue.front()); // 20
+console.log(queue.isEmpty()); // false
