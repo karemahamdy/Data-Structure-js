a85a27e3a47a4067bf1faf651d4095f658a1ced3
Karema <karemahamdy51@gmail.com>
2025-02-14 22:34:57 +0200

    add hashtable by map

--- Hash Table/hashTable.js
+++ Hash Table/hashTable.js
@@ -44,3 +44,44 @@ ht.set("age", 25);
 console.log(ht.get("name")); // Output: Alice
 ht.delete("name");
 console.log(ht.get("name")); // Output: undefined
+
+
+// but in js we have  Built in hash : Map() or object 
+
+const map = new Map();
+
+map.set("name", "Alice");
+map.set("age", 25);
+map.set("city", "Cairo");
+map.set(1, "Numeric Key"); 
+map.set(true, "Boolean Key"); 
+
+console.log("🚀 Map Content:", map);
+
+console.log(" Name:", map.get("name")); 
+console.log(" Age:", map.get("age")); 
+console.log(" City:", map.get("city")); 
+console.log(" Boolean Key:", map.get(true)); 
+
+console.log( map.has("age")); // true
+console.log(map.has("email")); // false
+
+map.delete("city");
+
+console.log( map.size); // 4
+
+for (let key of map.keys()) {
+  console.log( key);
+}
+
+for (let value of map.values()) {
+  console.log( value);
+}
+
+for (let [key, value] of map.entries()) {
+  console.log(` ${key} -> ${value}`);
+}
+
+map.clear();
+
+
