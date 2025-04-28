a70160b63eb7c65a98407e04459965a7193a5437
karemahamdy <karemahamdy51@gmail.com>
2025-04-28 12:46:00 +0300

    feat: 28. Find the Index of the First Occurrence in a String

---
+++ Two Pointer/28. Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,7 @@
+var strStr = function(haystack, needle) {
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+      if (haystack.substring(i, i + needle.length) === needle) {
+          return i;
+      } }
+      return -1
+};
